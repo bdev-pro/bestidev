c0cc9f8e633d031e193a5453824cd382a21ddb45
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-07-28 00:18:37 +0200

    show explicit variant price if it differs from master

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     # Shop by first option, less likely to chanche:
     Taxonomy.all.first.root.children.find_by_name(name)
   end
+  def variant_price_diff_format(variant)
+    return product_price(variant) unless variant.product.master.price
+    diff = product_price(variant, :format_as_currency => false) - product_price(variant.product, :format_as_currency => false)
+    if diff == 0
+      return nil
+    else
+      return ", #{t("price")}: " + product_price(variant)
+    end
+  end
 end
